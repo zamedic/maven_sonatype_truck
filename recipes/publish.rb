dcd6b068140427bd3cfbf360e99170acf8850864
Marc Arndt <marc@marcarndt.com>
2017-05-24 20:06:50 +0200

    adding delivery

---
+++ recipes/publish.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: build_cookbook
+# Recipe:: publish
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+secrets = get_project_secrets
+
+file '/tmp/gpg.key' do
+  content "#{secrets['gpg']}"
+  sensitive true
+end
+
+execute 'gpg2 --import /tmp/gpg.key' do
+  returns [0,2]
+end
+
+include_recipe 'coffee-truck::publish'
